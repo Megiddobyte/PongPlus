7dbde0227f0417f8105ff676fab92b8b7d06c2e6
Raymond <raymondjwallington@gmail.com>
2023-12-03 16:44:55 -0500

    small AI adjustments

--- Assets/Scripts/AIBrainWithTracking.cs
+++ Assets/Scripts/AIBrainWithTracking.cs
@@ -7,18 +7,15 @@ using Random = System.Random;
 public class AIBrainWithTracking : MonoBehaviour
 {
     private Rigidbody2D _rb;
-    private BoxCollider2D _bc;
     private Vector3 _puckPosition;
     private bool _findingPosition;
     [SerializeField, Range(10, 20)] private int _moveSpeed = 10;
-    [SerializeField, Range(10, 20)] private int _fastMoveSpeed = 20;
+    [SerializeField, Range(10, 20)] private int _fastMoveSpeed = 15;
     [SerializeField] private float _minYPos, _maxYPos;
-    private UnityTimer.Timer _timer;
     void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
-        _bc = GetComponent<BoxCollider2D>();
-        _timer = UnityTimer.Timer.Register(0.5f, TryGetPosition, isLooped: true);
+        UnityTimer.Timer.Register(0.5f, TryGetPosition, isLooped: true);
     }
 
     private void Move()
@@ -26,48 +23,37 @@ public class AIBrainWithTracking : MonoBehaviour
         var _yDistance = Mathf.Abs(_puckPosition.y - _rb.position.y);
         var _xDistance = Mathf.Abs(_puckPosition.x - _rb.position.x);
         var _direction = Mathf.Sign(_puckPosition.y - _rb.position.y);
-        if (_yDistance > 4)
+        
+        if (_xDistance > 30) { _rb.velocity = Vector2.zero; } //distances were just too small to adjust in time
+        if (_yDistance > 10 && _xDistance < 25) //for reference, paddles are exactly 30 units away from the center in the x direction
         {
-            UnityTimer.Timer.Register(0.5f, () => //small delay before moving directions for smoothing
+            UnityTimer.Timer.Register(0.2f, () => //small delay before moving directions for smoothing
             {
                 _rb.velocity = new Vector2(0, _fastMoveSpeed * _direction);
             });
         }
         else
         {
-            UnityTimer.Timer.Register(0.5f, () =>
+            UnityTimer.Timer.Register(0.2f, () =>
             {
-                if (UnityEngine.Random.Range(0, 2) == 1)
+                if (UnityEngine.Random.Range(0, 2) == 1) //can tweak this to increase odds of AI missing the bounce
                 {
-                    _rb.velocity = Vector2.zero;
+                    _rb.velocity = new Vector2(0, _moveSpeed * _direction);
                 }
                 else
                 {
-                    _rb.velocity = new Vector2(0, _moveSpeed * _direction);
+                    _rb.velocity = Vector2.zero;
                 }
             });
         }
     }
-    
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.GetComponent<Puck>())
-        {
-            
-        }
-        else //collided with wall
-        {
-            _rb.velocity = -_rb.velocity;
-        }
-    }
-
     private void FixedUpdate()
     {
         if (_findingPosition)
         {
-            //print("Moving...");
             Move();
-            _findingPosition = false;
+            UnityTimer.Timer.Register(0.2f, () => _findingPosition = false);
+            print($"y velocity is {_rb.velocity.y}");
         }
         if (transform.position.y >= _maxYPos)
         {
@@ -82,18 +68,9 @@ public class AIBrainWithTracking : MonoBehaviour
 
     private void TryGetPosition()
     {
-        //print("Trying to find position...");
         _findingPosition = true;
         var _cache = FindObjectOfType<Puck>();
-        if (_cache != null) {_puckPosition = _cache.gameObject.transform.position;}
-        else
-        {
-            _puckPosition = Vector2.zero;
-        }
-    }
-
-    private void OnDisable()
-    {
-        UnityTimer.Timer.Cancel(_timer);
+        if (_cache != null) { _puckPosition = _cache.gameObject.transform.position; }
+        else { _puckPosition = Vector2.zero; }
     }
 }
